320511fcd517aa88f5e5678799f3e22a69e9c9d7
Kazuki Nakai <na.kazuki08@gmail.com>
2020-06-24 09:57:56 +0900

    db chapter number []delete

--- db/migrate/20200616064246_add_number_to_chapters.rb
+++ db/migrate/20200616064246_add_number_to_chapters.rb
@@ -2,5 +2,5 @@ class AddNumberToChapters < ActiveRecord::Migration[5.2]
   def change
     add_column :chapters, :number, :integer
   end
-  add_index :chapters, [:number]
+  add_index :chapters, :number
 end
