3a1e8c30ab47725a9320264aaedcbf2340a8b8f1
Kazuki Nakai <na.kazuki08@gmail.com>
2020-10-03 17:00:57 +0900

    s

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 server "52.193.175.43", user: "Kazuki", roles: %w{app db web}
 
 set :ssh_options, {
-  keys: %w(~/.ssh/Output-Reading_key_rsa),
+  keys: %w(~/.ssh/output_reading_key_rsa),
   forward_agent: true
 }
