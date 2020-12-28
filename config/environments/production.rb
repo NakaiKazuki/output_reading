bb92a3bf4c0872da28f8cbc52cfe023630c86aa2
Kazuki Nakai <na.kazuki08@gmail.com>
2020-12-28 23:29:29 +0900

    sendgrid設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,15 +67,14 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'output-reading.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: Rails.application.credentials.host_server[:domain] }
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    port: ENV['MAILGUN_SMTP_PORT'],
     authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password:  ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: host,
     enable_starttls_auto: true
   }
 
