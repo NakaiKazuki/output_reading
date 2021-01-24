80bc92ade25b189d49378f4985f321f4a9832905
Kazuki Nakai <na.kazuki08@gmail.com>
2021-01-25 05:22:54 +0900

    修正

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
       provider: 'AWS',
       region: Rails.application.credentials.dig(:s3, :region),
       aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
-      aws_secret_access_key: Rails.application.credentials.dig(:aws, :aws_secret_access_key)
+      aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key)
     }
     config.fog_directory = Rails.application.credentials.dig(:s3, :bucket)
   end
